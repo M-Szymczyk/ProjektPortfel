1639eb038a492edd8ef24a70aad9e48abc416533
wolski0420 <wolski0420@gmail.com>
2022-02-05 01:29:58 +0100

    Optional to avoid exception

--- src/main/java/application/services/db/DataSeparator.java
+++ src/main/java/application/services/db/DataSeparator.java
@@ -4,7 +4,9 @@ import application.data.money.amount.AmountOfMoney;
 import application.data.money.bank.BankAccount;
 import application.data.user.UserData;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public class DataSeparator {
     public static List<AmountOfMoney> getAccounts(List<AmountOfMoney> amountOfMoneyList) {
@@ -12,7 +14,9 @@ public class DataSeparator {
     }
 
     public static List<UserData> getUsers(List<AmountOfMoney> amountOfMoneyList) {
-        return amountOfMoneyList.stream()
+        return Optional.ofNullable(amountOfMoneyList)
+                .stream()
+                .flatMap(Collection::stream)
                 .filter(BankAccount.class::isInstance)
                 .map(BankAccount.class::cast)
                 .map(BankAccount::getAccountHolder)
