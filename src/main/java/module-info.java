6dfaa49a43d5817d744df53331197b221167f561
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2024-07-03 16:29:13 +0200

    add module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module MoneyManagerConsoleAPP {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires java.desktop;
+    requires static lombok;
+    requires org.apache.commons.lang3;
+}
